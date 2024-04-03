e65e17e9d114758f8820b558cedea121e376b9d8
Walter Perdan <github@kalwaltart.it>
2024-04-03 12:59:34 +0200

    other CV methods

--- types/CV.d.ts
+++ types/CV.d.ts
@@ -1,3 +1,7 @@
+interface IPoint {
+    x: number;
+    y: number;
+}
 export declare class Image {
     private width;
     private height;
@@ -8,7 +12,30 @@ export declare class CV {
     static grayscale(imageSrc: ImageData, imageDst: ImageData): ImageData;
     static threshold(imageSrc: ImageData, imageDst: ImageData, threshold: number): ImageData;
     static adaptiveThreshold(imageSrc: ImageData, imageDst: ImageData, kernelSize: number, threshold: number): ImageData;
+    static otsu(imageSrc: ImageData): number;
     static stackBoxBlurMult: number[];
     static stackBoxBlurShift: number[];
     static stackBoxBlur(imageSrc: ImageData, imageDst: ImageData, kernelSize: number): ImageData;
+    static gaussianBlur(imageSrc: ImageData, imageDst: ImageData, imageMean: ImageData, kernelSize: number): ImageData;
+    static gaussianBlurFilter(imageSrc: ImageData, imageDst: ImageData, kernel: any[], horizontal: boolean): ImageData;
+    static gaussianKernel(kernelSize: number): number[];
+    static findContours(imageSrc: ImageData, binary: any): any[];
+    static borderFollowing(src: {
+        [x: string]: any;
+    }, pos: number, nbd: number, point: {
+        x: any;
+        y: any;
+    }, hole: boolean, deltas: any[]): any;
+    static neighborhood: number[][];
+    static neighborhoodDeltas(width: number): number[];
+    static approxPolyDP(contour: any, epsilon: number): IPoint[];
+    static warp(imageSrc: ImageData, imageDst: ImageData, contour: any, warpSize: number): ImageData;
+    static getPerspectiveTransform(src: IPoint[], size: number): number[];
+    static square2quad(src: IPoint[]): number[];
+    static isContourConvex(contour: IPoint[]): boolean;
+    static perimeter(poly: IPoint[]): number;
+    static minEdgeLength(poly: IPoint[]): number;
+    static countNonZero(imageSrc: ImageData, square: any): number;
+    static binaryBorder(imageSrc: ImageData, dst: any): any;
 }
+export {};
--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,16 @@ export declare namespace CV {
     const Image: typeof _Image;
     const grayscale: typeof _CV.grayscale;
     const threshold: typeof _CV.threshold;
+    const adaptiveThreshold: typeof _CV.adaptiveThreshold;
+    const otsu: typeof _CV.otsu;
+    const stackBoxBlur: typeof _CV.stackBoxBlur;
+    const gaussianBlur: typeof _CV.gaussianBlur;
+    const findContours: typeof _CV.findContours;
+    const approxPolyDP: typeof _CV.approxPolyDP;
+    const warp: typeof _CV.warp;
+    const getPerspectiveTransform: typeof _CV.getPerspectiveTransform;
+    const isContourConvex: typeof _CV.isContourConvex;
+    const perimeter: typeof _CV.perimeter;
+    const minEdgeLength: typeof _CV.minEdgeLength;
+    const countNonZero: typeof _CV.countNonZero;
 }
