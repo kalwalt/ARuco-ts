3e4de5e419fd68b5236d2f5db90d22fd57325d37
Walter Perdan <github@kalwaltart.it>
2024-04-03 12:59:34 +0200

    other CV methods

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,16 @@ export namespace CV {
     export const Image = _Image;
     export const grayscale = _CV.grayscale;
     export const threshold = _CV.threshold;
+    export const adaptiveThreshold = _CV.adaptiveThreshold;
+    export const otsu = _CV.otsu;
+    export const stackBoxBlur = _CV.stackBoxBlur;
+    export const gaussianBlur = _CV.gaussianBlur;
+    export const findContours = _CV.findContours;
+    export const approxPolyDP = _CV.approxPolyDP;
+    export const warp = _CV.warp;
+    export const getPerspectiveTransform = _CV.getPerspectiveTransform;
+    export const isContourConvex = _CV.isContourConvex;
+    export const perimeter = _CV.perimeter;
+    export const minEdgeLength = _CV.minEdgeLength;
+    export const countNonZero = _CV.countNonZero;
 }
