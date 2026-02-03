7de1a909f60fd647d9edbda14499c890a4061373
Walter Perdan <github@kalwaltart.it>
2026-02-03 17:35:24 +0100

    Refactor type definitions for consistency and clarity across math and SVD classes
    Signed-off-by: Walter Perdan <github@kalwaltart.it>

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -6,32 +6,32 @@ import { Pose as _Pose } from "./posit";
 import { Posit as _Posit } from "./posit";
 export declare const VERSION: string;
 export declare namespace CV {
-  const Image: typeof _Image;
-  const grayscale: typeof _CV.grayscale;
-  const threshold: typeof _CV.threshold;
-  const adaptiveThreshold: typeof _CV.adaptiveThreshold;
-  const otsu: typeof _CV.otsu;
-  const stackBoxBlur: typeof _CV.stackBoxBlur;
-  const gaussianBlur: typeof _CV.gaussianBlur;
-  const findContours: typeof _CV.findContours;
-  const approxPolyDP: typeof _CV.approxPolyDP;
-  const warp: typeof _CV.warp;
-  const getPerspectiveTransform: typeof _CV.getPerspectiveTransform;
-  const isContourConvex: typeof _CV.isContourConvex;
-  const perimeter: typeof _CV.perimeter;
-  const minEdgeLength: typeof _CV.minEdgeLength;
-  const countNonZero: typeof _CV.countNonZero;
+    const Image: typeof _Image;
+    const grayscale: typeof _CV.grayscale;
+    const threshold: typeof _CV.threshold;
+    const adaptiveThreshold: typeof _CV.adaptiveThreshold;
+    const otsu: typeof _CV.otsu;
+    const stackBoxBlur: typeof _CV.stackBoxBlur;
+    const gaussianBlur: typeof _CV.gaussianBlur;
+    const findContours: typeof _CV.findContours;
+    const approxPolyDP: typeof _CV.approxPolyDP;
+    const warp: typeof _CV.warp;
+    const getPerspectiveTransform: typeof _CV.getPerspectiveTransform;
+    const isContourConvex: typeof _CV.isContourConvex;
+    const perimeter: typeof _CV.perimeter;
+    const minEdgeLength: typeof _CV.minEdgeLength;
+    const countNonZero: typeof _CV.countNonZero;
 }
 export declare namespace ARuco {
-  const Detector: typeof _Detector;
+    const Detector: typeof _Detector;
 }
 export declare namespace SVD {
-  const svdcmp: typeof _SVD.svdcmp;
-  const pythag: typeof _SVD.pythag;
-  const sign: typeof _SVD.sign;
+    const svdcmp: typeof _SVD.svdcmp;
+    const pythag: typeof _SVD.pythag;
+    const sign: typeof _SVD.sign;
 }
 export declare namespace POS {
-  const Pose: typeof _Pose;
-  const Posit: typeof _Posit;
+    const Pose: typeof _Pose;
+    const Posit: typeof _Posit;
 }
 export { Vec3, Mat3 } from "./math";
--- types/src/posit.d.ts
+++ types/src/posit.d.ts
@@ -1,39 +1,25 @@
 import { IPoint, Vec3, Mat3 } from "./math-types";
 export declare class Posit {
-  model: Vec3[];
-  focalLength: number;
-  modelVectors: Mat3;
-  modelNormal: Vec3;
-  modelPseudoInverse: Mat3;
-  constructor(modelSize: number, focalLength: number);
-  buildModel(modelSize: number): Vec3[];
-  init(): void;
-  pose(points: IPoint[]): Pose;
-  pos(
-    points: IPoint[],
-    eps: Vec3,
-    rotation1: Mat3,
-    rotation2: Mat3,
-    translation1: Vec3,
-    translation2: Vec3
-  ): void;
-  iterate(points: IPoint[], rotation: Mat3, translation: Vec3): number;
-  getError(points: IPoint[], rotation: Mat3, translation: Vec3): number;
-  angle(a: IPoint, b: IPoint, c: IPoint): number;
+    model: Vec3[];
+    focalLength: number;
+    modelVectors: Mat3;
+    modelNormal: Vec3;
+    modelPseudoInverse: Mat3;
+    constructor(modelSize: number, focalLength: number);
+    buildModel(modelSize: number): Vec3[];
+    init(): void;
+    pose(points: IPoint[]): Pose;
+    pos(points: IPoint[], eps: Vec3, rotation1: Mat3, rotation2: Mat3, translation1: Vec3, translation2: Vec3): void;
+    iterate(points: IPoint[], rotation: Mat3, translation: Vec3): number;
+    getError(points: IPoint[], rotation: Mat3, translation: Vec3): number;
+    angle(a: IPoint, b: IPoint, c: IPoint): number;
 }
 export declare class Pose {
-  bestError: number;
-  bestRotation: number[][];
-  bestTranslation: number[];
-  alternativeError: number;
-  alternativeRotation: number[][];
-  alternativeTranslation: number[];
-  constructor(
-    error1: number,
-    rotation1: number[][],
-    translation1: number[],
-    error2: number,
-    rotation2: number[][],
-    translation2: number[]
-  );
+    bestError: number;
+    bestRotation: number[][];
+    bestTranslation: number[];
+    alternativeError: number;
+    alternativeRotation: number[][];
+    alternativeTranslation: number[];
+    constructor(error1: number, rotation1: number[][], translation1: number[], error2: number, rotation2: number[][], translation2: number[]);
 }
